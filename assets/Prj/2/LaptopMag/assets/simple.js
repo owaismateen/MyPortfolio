0471ae7441ee0adff954492f8eb12cd74e3549cf
Owais <113190230+owaismateen@users.noreply.github.com>
2023-08-12 00:51:22 +0500

    files upload

---
+++ assets/Prj/2/LaptopMag/assets/simple.js
@@ -0,0 +1,7 @@
+// Handle form submission
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    // Perform form validation or submit data to server using AJAX
+    // You can customize this part based on your requirements
+    alert('Form submitted!');
+  });
